92ff52fec7e72c1d2a724196d8d490e5059e516e
panda <p4nd4@posteo.de>
2024-04-09 13:55:27 +0200

    getCardsData: error handling

--- db.js
+++ db.js
@@ -16,6 +16,9 @@ export const getCardsData = async (req, res) => {
     try {
         const cardsData = await Card.find();
         console.log(cardsData);
+        if (res.status === 404) {
+            return res.status(404).json({ message: "No data found" });
+        }
         res.status(200).json(cardsData);
     } catch (error) {
         res.status(404).json({ message: error.message });
