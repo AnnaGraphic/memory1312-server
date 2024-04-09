877326e24f9cfb67782c4de812ba12c93f81ac49
panda <p4nd4@posteo.de>
2024-04-09 10:35:32 +0200

    server.js: refactor, add middleware, set routes

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
+import cors from 'cors';
 import express from 'express';
 import env from 'dotenv';
+import { getCardsData } from './db.js';
 env.config();
- const { PORT } = process.env;
 
+// ----- config -----
 const app = express();
+const { PORT, URI } = process.env;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
-  }
-);
+// ----- middleware -----
+app.use(cors());
+app.use(express.json());
+
+// ----- routes -----
+app.get('/api/cards', getCardsData);
 
 app.listen(PORT, () => {
   console.log(`server is running on port ${PORT}`);
